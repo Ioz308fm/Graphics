cf022316191abbfc265df1b1e1241e679bea2332
Ioz308fm <evtukhov01@gmail.com>
2025-03-20 09:59:31 +0300

    Add files via upload

---
+++ Laba3/Laba3/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(763, 38);
+            this.trackBar3.Maximum = 360;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(350, 56);
+            this.trackBar3.TabIndex = 8;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(392, 38);
+            this.trackBar2.Maximum = 360;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(350, 56);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 38);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(350, 56);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button2.Location = new System.Drawing.Point(1149, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 45);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Принять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox2.Location = new System.Drawing.Point(1196, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 32);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(1146, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 26);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "x=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(1146, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 26);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "y=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox1.Location = new System.Drawing.Point(1196, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label3.Location = new System.Drawing.Point(1146, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 26);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "z=";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox3.Location = new System.Drawing.Point(1196, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 32);
+            this.textBox3.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button1.Location = new System.Drawing.Point(1151, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 45);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Рисовать Безье";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1128, 757);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button3.Location = new System.Drawing.Point(1151, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 45);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Рисовать B-слайн 2п";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button4.Location = new System.Drawing.Point(1149, 411);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(241, 45);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Рисовать B-слайн 3п";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label4.Location = new System.Drawing.Point(97, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 26);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Вращение по OX";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label5.Location = new System.Drawing.Point(487, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(186, 26);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Вращение по OY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label6.Location = new System.Drawing.Point(845, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 26);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Вращение по OZ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 841);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ lab 9/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Color> colors = new List<Color>();
+        const int resx = 1920, resy = 1080;
+        double boom = 4.0;
+        int N = 5000;
+        const double s = 1.0 / 400.0;//scaling
+        Bitmap map = new Bitmap(resx, resy);
+        int n = 0;
+
+
+        void redupfill()
+        {
+            int K = (int)Math.Pow(N + 1, 1.0/ 3.0);
+            for (int i=0;i<K;i++)
+            {
+                for (int j=0;j<K;j++)
+                {
+                    for(int m=0;m<K ;m++)
+                    {
+                        Color a = new Color();
+                        a = Color.FromArgb(255*i/K, 255 - 255 * j / K, 255 - 255 * m / K);
+                        colors.Add(a);
+                    }
+                }
+            }
+            while (colors.Count<101)
+            {
+                Color a = new Color();
+                a = Color.FromArgb(colors.Count*255/101 ,0,0);
+                colors.Add(a);
+            }
+        }
+
+        void bluedownfill()
+        {
+            int K = (int)Math.Pow(N + 1, 1.0 / 3.0);
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    for (int m = 0; m < K; m++)
+                    {
+                        Color a = new Color();
+                        a = Color.FromArgb(255 * i / K, 255 - 255 * j / K, 255 - 255 * m / K);
+                        colors.Add(a);
+                    }
+                }
+            }
+            while (colors.Count < 101)
+            {
+                Color a = new Color();
+                a = Color.FromArgb(colors.Count * 255 / 101, 0, 0);
+                colors.Add(a);
+            }
+        }
+
+        double dwell(double cx, double cy,double xs,double ys)
+        {
+
+            double sx = xs - resx / 2,sy= ys - resy / 2; ;
+            sx *= s;
+            sy *= s;
+            double tmp, dx = sx, dy = sy, f = dx * dx + dy * dy;
+            for (int i = 0; i <= N && Math.Abs(f) <= boom; i++)
+            {
+                tmp = dx;
+                dx = dx * dx - dy * dy + cx;
+                dy = 2 * tmp * dy + cy;
+                f = dx * dx + dy * dy;
+            }
+
+            return f;
+        }
+
+        int dwellnum(double cx, double cy, double xs, double ys)
+        {
+            double sx = xs - resx / 2, sy = ys - resy / 2; ;
+            sx *= s;
+            sy *= s;
+            int i=0;
+            double tmp, dx = sx, dy = sy, f = dx * dx + dy * dy;
+            for (i = 0; i <= N && Math.Abs(f) <= boom; i++)
+            {
+                tmp = dx;
+                dx = dx * dx - dy * dy + cx;
+                dy = 2 * tmp * dy + cy;
+                f = dx * dx + dy * dy;
+            }
+
+            return i;
+        }
+
+        void paint(int i, int j,int k)
+        {
+            if(k==1)
+            {
+                double cx = -0.74543;
+                double cy = 0.11301;
+                int M = dwellnum(cx, cy, i, j);
+                if (M >= 101)
+                    map.SetPixel(i, j, Color.Red);
+                else
+                    map.SetPixel(i, j, colors[M]);
+            }
+            if (k == 2)
+            {
+                double cx = -0.70176;
+                double cy = -0.3842;
+                int M = dwellnum(cx, cy, i, j);
+                if (M >= 101)
+                    map.SetPixel(i, j, Color.Red);
+                else
+                    map.SetPixel(i, j, Color.FromArgb(255, (M * 12) % 255, 0, (M * 12) % 255));
+            }
+        }
+
+        void julia(int k)
+        {
+            if (k == 1)
+                redupfill();
+            for (int i=0;i<resx;i++)
+            {
+                for(int j=0;j<resy;j++)
+                {
+                    paint (i,j,k);
+                }
+            }
+            pictureBox1.Image = map;
+        }
+
+       
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                n++;
+            }
+            else
+            {
+                n--;
+            }
+            
+            julia(n);
+        }
+
+        PointF f1, f2;
+    }
+}
